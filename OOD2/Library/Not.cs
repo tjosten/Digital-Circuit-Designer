9e531f73e0b775524de524c2f2712419e11ae3f0
Timo Josten <tj@mkswap.net>
2013-05-06 14:51:37 +0200

    Introducing TestControl
    I have no idea what I'm doing

---
+++ OOD2/Library/Not.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD2.Library
+{
+    class Not : OOD2.Library.Interfaces.Gate
+    {
+
+        private List<OOD2.Library.Input> inputs;
+
+        public Not()
+        {
+            // the and-switch has a maximum of 2 inputs
+            this.inputs = new List<OOD2.Library.Input>(2);
+        }
+
+        public bool run()
+        {
+            // we assume the outcome is true, until proven otherwise
+
+            // check if we have any input connected
+            if (this.inputs.Count == 0)
+            {
+                throw new Exception("The NOT-Gate needs to have at least 1 input connected in order to work!");
+            }
+
+            // iterate the inputs
+            foreach (OOD2.Library.Input input in this.inputs)
+            {
+                if (input.currentState == true)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ OOD2/TestControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+
+
+namespace OOD2
+{
+    public partial class TestControl : PictureBox
+    {
+        private Image image;
+
+        public TestControl()
+        {
+            this.init();
+        }
+
+        void init()
+        {
+            image = Image.FromFile("C:\\Users\\tj\\Documents\\GitHub\\OOD2-Assignment\\OOD2\\img\\and.jpg");
+            this.Image = image;
+            this.Height = image.Height;
+            this.Width = image.Width;
+        }
+    }
+}
